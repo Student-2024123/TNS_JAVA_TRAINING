101f92678add1f14e3d4cca88a9c244e80c43405
Dipesh Morankar <morankardipesh@gmail.com>
2026-08-03 17:42:25 +0000

    Assignment 2 Done

---
+++ Assignment2/Employee.java
@@ -0,0 +1,11 @@
+package Assignment2;
+public class Employee {
+
+    Employee() {
+        System.out.println("Student Object Created");
+    }
+
+    public static void main(String[] args) {
+        Employee s1 = new Employee();
+    }
+}
