44ee9ea9f10c2cc8fadae82d1db5d6b078a1b707
morankardipesh <morankardipesh@gmail.com>
2026-08-16 17:35:42 +0530

    Done

--- Assignment5/TicketBooking.java
+++ Assignment5/TicketBooking.java
@@ -4,18 +4,15 @@ public class TicketBooking {
     private String customer;
     private Integer noOfSeats;
 
-    // Default constructor
     public TicketBooking() {
     }
 
-    // Parameterized constructor
     public TicketBooking(String stageEvent, String customer, Integer noOfSeats) {
         this.stageEvent = stageEvent;
         this.customer = customer;
         this.noOfSeats = noOfSeats;
     }
 
-    // Getters
     public String getStageEvent() {
         return stageEvent;
     }
@@ -28,7 +25,6 @@ public class TicketBooking {
         return noOfSeats;
     }
 
-    // Setters
     public void setStageEvent(String stageEvent) {
         this.stageEvent = stageEvent;
     }
@@ -41,7 +37,6 @@ public class TicketBooking {
         this.noOfSeats = noOfSeats;
     }
 
-    // Cash payment
     public void makePayment(Double amount) {
         System.out.println("Stage event:" + stageEvent);
         System.out.println("Customer:" + customer);
@@ -49,7 +44,6 @@ public class TicketBooking {
         System.out.printf("Amount %.1f paid in cash%n", amount);
     }
 
-    // Wallet payment
     public void makePayment(String walletNumber, Double amount) {
         System.out.println("Stage event:" + stageEvent);
         System.out.println("Customer:" + customer);
@@ -58,7 +52,6 @@ public class TicketBooking {
                 amount, walletNumber);
     }
 
-    // Credit card payment
     public void makePayment(String creditCard, String ccv,
             String name, Double amount) {
         System.out.println("Stage event:" + stageEvent);
